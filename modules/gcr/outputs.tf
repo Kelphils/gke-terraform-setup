d263d6592f276313c142cc6dd6abb0c97b85c267
Kelvin Obioha <obiohakelvin96@gmail.com>
2022-08-21 12:01:51 +0100

    update workflow file

--- modules/gcr/outputs.tf
+++
@@ -1,4 +0,0 @@
-# output "google_artifact_registry_repository_name" {
-#   value       = google_artifact_registry_repository.repo.name
-#   description = "The name of the Artifact Registry Repository"
-# }
